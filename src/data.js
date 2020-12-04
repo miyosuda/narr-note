4a2443cac12294ba130c51c445d15e4fc53ac7d2
kosuke miyoshi <miyosuda@gmail.com>
2020-12-05 04:36:11 +0900

    rename nodeDatas, pageDatas -> nodes, pages in json data

--- src/data.js
+++ src/data.js
@@ -1,109 +1,5 @@
 const {clone, cloneArray} = require('./utils')
 
-
-class PageData {
-  constructor() {
-    this.nodeDatas = []
-  }
-
-  getNodeDatas() {
-    return this.nodeDatas
-  }
-
-  fromRaw(rawData) {
-    this.nodeDatas = []
-    const rawNodeDatas = rawData.nodeDatas 
-    rawNodeDatas.forEach(rawNodeData => {
-      const nodeData = new NodeData()
-      nodeData.fromRaw(rawNodeData)
-      this.nodeDatas.push(nodeData)
-    })
-  }
-
-  addNode(nodeData) {
-    this.nodeDatas.push(nodeData)
-  }
-
-  removeNode(nodeData) {
-    const nodeIndex = this.nodeDatas.indexOf(nodeData)
-    if(nodeIndex >= 0) {
-      this.nodeDatas.splice(nodeIndex, 1)
-    } else {
-      console.log('node data not found')
-    }
-  }
-
-  clone() {
-    const clonedPageData = new PageData()
-    clonedPageData.nodeDatas = cloneArray(this.nodeDatas)
-    return clonedPageData
-  }
-}
-
-
-const DATA_VERSION = 1
-
-class NoteData {
-  constructor() {
-    this.version = DATA_VERSION
-    
-    this.pageDatas = []
-    const pageData = new PageData()
-    this.pageDatas.push(pageData)
-    this.currentPage = 0    
-  }
-
-  addNode(nodeData) {
-    const pageData = this.pageDatas[this.currentPage]
-    pageData.addNode(nodeData)
-  }
-
-  removeNode(nodeData) {
-    const pageData = this.pageDatas[this.currentPage]
-    pageData.removeNode(nodeData)
-  }
-
-  clone() {
-    const clonedNoteData = new NoteData()
-    
-    clonedNoteData.currentPage = this.currentPage
-    clonedNoteData.pageDatas = []
-    
-    this.pageDatas.forEach(pageData => {
-      clonedNoteData.pageDatas.push(pageData.clone())
-    })
-    return clonedNoteData
-  }
-
-  getCurretNodeDatas() {
-    const pageData = this.pageDatas[this.currentPage]
-    return pageData.getNodeDatas()
-  }
-
-  toJson() {
-    const json = JSON.stringify(this, null , '\t')
-    return json
-  }
-
-  fromJson(json) {
-    this.version = DATA_VERSION // vesionは現在のバージョンを利用する
-    
-    const rawData = JSON.parse(json)
-    this.currentPage = rawData.currentPage
-    
-    const rawPageDatas = rawData.pageDatas
-    this.pageDatas = []
-    
-    rawPageDatas.forEach(rawPageData => {
-      const pageData = new PageData()
-      pageData.fromRaw(rawPageData)
-      this.pageDatas.push(pageData)
-    })
-  }
-}
-
-
-
 const NODE_TYPE_NONE = 0
 const NODE_TYPE_TEXT = 1
 const NODE_TYPE_RECT = 2
@@ -269,6 +165,109 @@ class NodeData {
     }
   }
 }
+
+
+class PageData {
+  constructor() {
+    this.nodes = [] // NodeData[]
+  }
+
+  getNodeDatas() {
+    return this.nodes
+  }
+
+  fromRaw(rawData) {
+    this.nodes = []
+    const rawNodeDatas = rawData.nodes 
+    rawNodeDatas.forEach(rawNodeData => {
+      const nodeData = new NodeData()
+      nodeData.fromRaw(rawNodeData)
+      this.nodes.push(nodeData)
+    })
+  }
+
+  addNode(nodeData) {
+    this.nodes.push(nodeData)
+  }
+
+  removeNode(nodeData) {
+    const nodeIndex = this.nodes.indexOf(nodeData)
+    if(nodeIndex >= 0) {
+      this.nodes.splice(nodeIndex, 1)
+    } else {
+      console.log('node data not found')
+    }
+  }
+
+  clone() {
+    const clonedPageData = new PageData()
+    clonedPageData.nodes = cloneArray(this.nodes)
+    return clonedPageData
+  }
+}
+
+
+const DATA_VERSION = 1
+
+class NoteData {
+  constructor() {
+    this.version = DATA_VERSION
+    
+    this.pages = []
+    const pageData = new PageData()
+    this.pages.push(pageData)
+    this.currentPage = 0    
+  }
+
+  addNode(nodeData) {
+    const pageData = this.pages[this.currentPage]
+    pageData.addNode(nodeData)
+  }
+
+  removeNode(nodeData) {
+    const pageData = this.pages[this.currentPage]
+    pageData.removeNode(nodeData)
+  }
+
+  clone() {
+    const clonedNoteData = new NoteData()
+    
+    clonedNoteData.currentPage = this.currentPage
+    clonedNoteData.pages = []
+    
+    this.pages.forEach(pageData => {
+      clonedNoteData.pages.push(pageData.clone())
+    })
+    return clonedNoteData
+  }
+
+  getCurretNodeDatas() {
+    const pageData = this.pages[this.currentPage]
+    return pageData.getNodeDatas()
+  }
+
+  toJson() {
+    const json = JSON.stringify(this, null , '\t')
+    return json
+  }
+
+  fromJson(json) {
+    this.version = DATA_VERSION // vesionは現在のバージョンを利用する
+    
+    const rawData = JSON.parse(json)
+    this.currentPage = rawData.currentPage
+    
+    const rawPageDatas = rawData.pages
+    this.pages = []
+    
+    rawPageDatas.forEach(rawPageData => {
+      const pageData = new PageData()
+      pageData.fromRaw(rawPageData)
+      this.pages.push(pageData)
+    })
+  }
+}
+
 
 module.exports = {
   NODE_TYPE_NONE,
